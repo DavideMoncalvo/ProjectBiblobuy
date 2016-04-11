5b198d4e425e9683f943aa1bee86c36b0747b3d1
unknown <Stagiaire@R203-3.demostraining.com>
2016-04-11 16:28:00 +0200

    commit : ajouter livre

--- src/DM/demos/TestBiblo.java
+++ src/DM/demos/TestBiblo.java
@@ -9,11 +9,14 @@ public class TestBiblo {
 
 		Article l1 = new Livre(10, 2, "fabulous", "25852582", "splendore", "margaret mazzantini", "mondadori",
 				"romanzo");
+		Article l2 = new Livre(10, 2, "fabulous", "25852582", "splendore", "margaret mazzantini", "mondadori",
+				"romanzo");
 		Article d1 = new DvD(12, 3, "larmes au yeaux", "125252", "Titanic", "i don't know", "DiCaprio, Winslet",
 				"love story");
 		Article c1 = new CD(9.99, 1, "spacca", "969582", "Me", "Jane", "Virgin", "Hindi");
 		Panier panier = new Panier();
 		panier.ajouterArticle(l1);
+		panier.ajouterArticle(l2);
 		panier.ajouterArticle(d1);
 		panier.ajouterArticle(c1);
 		System.out.println(panier);
