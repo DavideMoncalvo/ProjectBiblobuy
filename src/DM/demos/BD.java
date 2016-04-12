c25d66d2309498835f99b03dddd8063c9aac62a4
unknown <Stagiaire@R203-3.demostraining.com>
2016-04-12 10:23:49 +0200

    branche 2

--- src/DM/demos/BD.java
+++ src/DM/demos/BD.java
@@ -1,5 +1,11 @@
 package DM.demos;
 
-public class BD {
+public class BD extends Livre {
+
+	public BD(double prixHT, int stock, String description, String reference, String title, String auteur,
+			String editeur, String genre) {
+		super(prixHT, stock, description, reference, title, auteur, editeur, genre);
+		
+	}
 
 }
